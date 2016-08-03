805b19850636e21d83cb1977ebb3b78cb0e469d4
Brian Jackson <bjack205@gmail.com>
2016-08-03 10:53:20 -0600

    Make quatconj work with 2D quat arrays

--- quatconj.m
+++ quatconj.m
@@ -3,11 +3,11 @@ function q = quatconj(q)
 %   quaternions of the form [r, ai, bj, ck]
 %
 %   INPUTS:
-%       q is an Mx4 quaternion array
+%       q is an Mx4 quaternion vector or Mx4xN quaternion array
 %
 %   OUTPUT:
-%       q0 is an Mx4 quaternion array
+%       q0 is an Mx4 quaternion vector or Mx4xN quaternion array
 % 
 % Brian Jackson July 2016
 % Brigham Young University
-q(:,2:4) = -1*q(:,2:4);
+q(:,2:4,:) = -1*q(:,2:4,:);
